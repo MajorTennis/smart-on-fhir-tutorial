80da7a19d731056c679a886b7d5b7fc1d3e11a46
Martin Jones <martin34581@gmail.com>
2021-06-03 16:10:07 +0100

    allergies

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -94,6 +94,7 @@
 
                     p.hdl = getQuantityValueAndUnit(hdl[0]);
                     p.ldl = getQuantityValueAndUnit(ldl[0]);
+                    p.allergies = allergyTable;
 
                     ret.resolve(p);
                 });
