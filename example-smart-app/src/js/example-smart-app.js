89f90d935cde268fc7b61980088282fc04358a51
Martin Jones <martin34581@gmail.com>
2021-06-03 14:08:13 +0100

    vital signs

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -23,8 +23,9 @@
                                 'http://loinc.org|2089-1',
                                 'http://loinc.org|55284-4'
                             ]
-                        } /*,
-                        date: 'gt2021-01-01'*/
+                        },
+                        date: 'gt2021-01-01',
+                        category: 'vital-signs'
                     }
                 });
 
